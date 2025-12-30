72194dab9405e066a65f8e4dd20574ad4cb60864
riky007-ux <mjvphbswfs@privaterelay.appleid.com>
2025-12-29 21:32:18 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF8B7B',
+        secondary: '#C7B5A3',
+        background: '#F8F6F4',
+        card: '#FAF7F4',
+        text: {
+          dark: '#4B3F38',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
