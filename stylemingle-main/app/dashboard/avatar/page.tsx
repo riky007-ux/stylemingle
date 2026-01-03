c6a177aef796a6069a6011235d649439a1e9d52f
riky007-ux <mjvphbswfs@privaterelay.appleid.com>
2026-01-03 22:35:00 +0000

    chore: stabilize avatar page

--- stylemingle-main/app/dashboard/avatar/page.tsx
+++ stylemingle-main/app/dashboard/avatar/page.tsx
@@ -1,5 +1,9 @@
 'use client';
 
 export default function AvatarPage() {
-  return <></>;
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-semibold">Your Avatar</h1>
+    </div>
+  );
 }
