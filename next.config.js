3bea0ea9d59b171132c5f63189335d66a12e2b44
riky007-ux <mjvphbswfs@privaterelay.appleid.com>
2026-01-06 05:42:40 +0000

    chore: commit uncommitted changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
 };
 
