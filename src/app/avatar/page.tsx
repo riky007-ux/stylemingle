e7257282826538ab80210a0c5db4b90a840ecd14
riky007-ux <mjvphbswfs@privaterelay.appleid.com>
2026-01-13 10:01:14 -0800

    feat: update avatar page to showcase soft-smile expression

--- src/app/avatar/page.tsx
+++ src/app/avatar/page.tsx
@@ -1,11 +1,10 @@
 'use client';
-
 import Avatar from '@/components/avatar/Avatar';
 
 export default function AvatarPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Avatar size="M" />
+      <Avatar size="M" expression="soft-smile" />
     </div>
   );
 }
