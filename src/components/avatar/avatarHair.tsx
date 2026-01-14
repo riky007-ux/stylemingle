d29b27b34399704eecd2ea26eddc2b4d1afd006f
riky007-ux <mjvphbswfs@privaterelay.appleid.com>
2026-01-13 21:41:37 -0800

    feat: darken hair color to enhance avatar presence

--- src/components/avatar/avatarHair.tsx
+++ src/components/avatar/avatarHair.tsx
@@ -15,22 +15,22 @@ export const avatarHair: Record<AvatarHairKey, Record<AvatarSize, React.ReactNod
   short: {
     S: (
       <g id="hair-short-s">
-        <rect x={80} y={0} width={40} height={25} fill="#6d4c41" rx={10} />
+        <rect x={80} y={0} width={40} height={25} fill="#5a3e34" rx={10} />
       </g>
     ),
     M: (
       <g id="hair-short-m">
-        <rect x={75} y={0} width={50} height={30} fill="#6d4c41" rx={10} />
+        <rect x={75} y={0} width={50} height={30} fill="#5a3e34" rx={10} />
       </g>
     ),
     L: (
       <g id="hair-short-l">
-        <rect x={70} y={0} width={60} height={35} fill="#6d4c41" rx={10} />
+        <rect x={70} y={0} width={60} height={35} fill="#5a3e34" rx={10} />
       </g>
     ),
     XL: (
       <g id="hair-short-xl">
-        <rect x={65} y={0} width={70} height={40} fill="#6d4c41" rx={10} />
+        <rect x={65} y={0} width={70} height={40} fill="#5a3e34" rx={10} />
       </g>
     ),
   },
